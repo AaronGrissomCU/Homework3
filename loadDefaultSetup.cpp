9d32ca6db08a82b86b52d2c4599aa542331bf425
AaronGrissomCU <32560635+AaronGrissomCU@users.noreply.github.com>
2017-10-08 20:51:00 -0600

    Update loadDefaultSetup.cpp
    Don't forget the started code, this passes all tests too.

--- loadDefaultSetup.cpp
+++ loadDefaultSetup.cpp
@@ -1,33 +1,24 @@
 city* loadDefaultSetup(city *head)
 {
-		city* LosAngeles = new city;
-		city* Phoenix = new city;
-		city* Denver = new city;
-		city* Dallas = new city;
-		city* Atlanta = new city;
-		city* NewYork = new city;
-		
-		LosAngeles->name = "Los Angeles";
-		Phoenix->name = "Phoenix";
-		Denver->name = "Denver";
-		Dallas->name = "Dallas";	
-		Atlanta->name = "Atlanta";
-		NewYork->name = "New York";
-		
-		head = LosAngeles;
-		LosAngeles->next = Phoenix;
-		Phoenix->next = Denver;
-		Denver->next = Dallas;
-		Dallas->next = Atlanta;
-		Atlanta->next = NewYork;
-		NewYork->next = NULL;
-		
-		LosAngeles->numberMessages = 0;
-		Phoenix->numberMessages = 0;
-		Denver->numberMessages = 0;
-		Dallas->numberMessages = 0;	
-		Atlanta->numberMessages = 0;
-		NewYork->numberMessages = 0;
-		
-		return head;
+    city* prev;
+    
+    head = deleteEntireNetwork(head);
+    head = addCity(head,NULL,"Los Angeles");
+    
+    prev = searchNetwork(head, "Los Angeles");
+    head = addCity(head,prev,"Phoenix");
+    
+    prev = searchNetwork(head, "Phoenix");
+    head = addCity(head,prev,"Denver");
+    
+    prev = searchNetwork(head, "Denver");
+    head = addCity(head,prev,"Dallas");
+    
+    prev = searchNetwork(head, "Dallas");
+    head = addCity(head,prev,"Atlanta");
+    
+    prev = searchNetwork(head, "Atlanta");
+    head = addCity(head,prev,"New York");
+	
+	return head;
 }
